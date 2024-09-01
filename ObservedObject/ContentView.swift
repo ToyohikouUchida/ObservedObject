2334705444efcda9e8e5ae7456845d3cc5b890bd
ToyohikoUchida <tyhkucd@gmail.com>
2024-09-01 10:20:04 +0900

    Initial Commit

---
+++ ObservedObject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ObservedObject
+//
+//  Created by 内田豊彦 on 2024/09/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ObservedObject/ObservedObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservedObjectApp.swift
+//  ObservedObject
+//
+//  Created by 内田豊彦 on 2024/09/01.
+//
+
+import SwiftUI
+
+@main
+struct ObservedObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
